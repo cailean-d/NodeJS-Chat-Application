a53d42f452ba70e76bfffdde26fc47d4414f6f9a
Denis Lykov <cailean393@gmail.com>
2017-09-02 02:17:37 +0300

    added sound notification

--- public/js/socket/global.js
+++ public/js/socket/global.js
@@ -5,12 +5,15 @@
     const socket = io({ query: { id: myID, nickname: myName } });
 
     socket.on('added_to_friends', function(data){
+        document.getElementById('notif').play();
         alert('you are added to friend by ' + data)
     })
     socket.on('deleted_from_friends', function(data){
+        document.getElementById('notif').play();        
         alert('you are deleted from friends by ' + data)
     })
     socket.on('friendship_rejected', function(data){
+        document.getElementById('notif').play();                
         alert('friendship rejected by ' + data)
     })
 })();
